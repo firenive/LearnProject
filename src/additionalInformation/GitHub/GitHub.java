7114e312bd21c0ffa8f76c1ea98114c8db85c31f
firenive <firenive@gmail.com>
2020-05-23 20:16:56 +0300

    gitignore

--- src/additionalInformation/GitHub/GitHub.java
+++ src/additionalInformation/GitHub/GitHub.java
@@ -1,4 +1,23 @@
 package additionalInformation.GitHub;
 
 public class GitHub {
+    // https://www.youtube.com/playlist?list=PLuY6eeDuleIOMB2R_Kky05ZfiAx2_pbAH
+    // Команды Git
+
+    // Установка имени и почты пользователя
+    // git config --global user.name firenive
+    // git config --global user.email firenive@gmail.com
+
+    // Создание репозитория git init
+    // Добавление всех файлов в репозиторий
+
+    // Первый коммит в локальный репозиторий git commit -m "first commit"
+    // Соединение с gitHub git remote add origin https://github.com/firenive/LearnProject.git
+    // Push в основную ветвь (master) git push -u origin master
+
+    // Contributions - кто имеет доступ к проекту и может делать в нем изменения. Новые пользователи добавляются в настройках
+    // Collaborations. Далее копируем для пользователя ссылку доступа и отправляем на почту
+
+    // Для клонирования репозитория используем команду git clone https://github.com/jquery/jquery.git (как пример)
+
 }
